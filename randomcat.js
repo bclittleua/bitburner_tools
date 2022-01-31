f6a9bbfe369800147ffb41844e7b5bc4c21de840
bclittleua <32556428+bclittleua@users.noreply.github.com>
2022-01-31 13:59:22 -0700

    Create randomcat.js

---
+++ randomcat.js
@@ -0,0 +1,10 @@
+/** @param {NS} ns **/
+// run with this alias: alias -g kitty="run randomcat.js;cat kitty.txt"
+// fetches and displays a random cat image, lol.
+
+export async function main(ns) {
+await ns.wget("https://api.thecatapi.com/v1/images/search", "json.txt");
+var content = await ns.read("json.txt");
+var url = JSON.parse(content)[0]["url"];
+await ns.write("kitty.txt", `<html><body><img src=\"${url}\"></img></body></html>`, "w");
+}
