91757fd4c89d12d84fa06a2fb5aa82a68507b00a
bclittleua <32556428+bclittleua@users.noreply.github.com>
2022-01-31 15:28:46 -0700

    Update randomcat.js

--- randomcat.js
+++ randomcat.js
@@ -3,7 +3,7 @@
 // fetches and displays a random cat image, lol.
 
 export async function main(ns) {
-await ns.wget("https://api.thecatapi.com/v1/images/search", "json.txt");
+await ns.wget("https://api.thecatapi.com/v1/images/search?mim_types=gif", "json.txt"); //remove the ?mim_types=gif or change to png
 var content = await ns.read("json.txt");
 var url = JSON.parse(content)[0]["url"];
 await ns.write("kitty.txt", `<html><body><img src=\"${url}\"></img></body></html>`, "w");
